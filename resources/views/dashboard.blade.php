c66932d2c64a1b912b10938542e1d99858eb70d0
Rafał Nowak <rafal.robi.nowak@gmail.com>
2021-04-22 13:48:56 +0200

    Przejście na W3.CSS i aktualizacja widoku dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,38 +1,113 @@
-<!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<!DOCTYPE html>
+<html lang="en">
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>Dashboard - {{ config('app.name', 'Laravel') }}</title>
-
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!-- Przekazywanie token'u -->
+  <meta name="csrf-token" content="{{ csrf_token() }}">
+  <!-- Podstawianie odpowiedniej tytułu strony -->
+  <title>@yield('page_title','CBM')</title>
+  <!-- Blok Style -->
+  <!-- CSS z W3 Schoole -->
+  <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+  <!-- Fonty wykorzystane na stronie -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Raleway&family=Roboto">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <!-- modyfikator stylu -->
+  <link rel="stylesheet" href="{{asset('css/app.css')}}">
 </head>
-<body>
-    <div id="app">
-        <div class="">
-            @include('static.topbar')
-        </div>
-        <div class="row" role="main">
-            <aside class="col-2" style="background-color: rgba(0, 0, 0, 0.2)">
-              @include('static.sidebar')
-            </aside>
-            <main class="col">
-                @yield('content')
-            </main>
-        </div>
-        @include('static.footer')
+<body class="w3-light-grey">
+  <!--Górny pasek z logo i przyciskiem na menu -->
+  <div class="w3-bar w3-top w3-black w3-large top-bar">
+    <button class="w3-bar-item w3-button w3-hide-large w3-hover-none w3-hover-text-light-grey" onclick="w3_open();"><i class="fa fa-bars"></i>Menu</button>
+    <span class="w3-bar-item w3-right">WWP-CBM</span>
+  </div> 
+
+  <!-- Menu boczne -->
+  <nav class="w3-sidebar w3-collapse w3-white w3-animate-left sidebar-menu" id="mySidebar">
+    <br/>
+    <div class="w3-container w3-row">
+      <div class="w3-col s4">
+        <img src="{{asset('img/avatar.png')}}" class="w3-circle w3-margin-right avatar-img" alt="avatar"> <!--FIXME: do zmiany na podlinkowanie z danych o użytkowniku-->
+      </div>
+      <div class="w3-col s8 w3-bar">
+        <span>Witaj, <strong>{{ Auth::user()->name, "Nieznajomy" }}</strong></span>
+        <br>
+        <a href="" class="w3-bar-item w3-button"><i class="fa fa-envelope"></i></a>
+        <a href="" class="w3-bar-item w3-button"><i class="fa fa-user"></i></a> <!-- FIXME: przemyśleć nad zmianą na 'button' i obsługą modala z informacjami-->
+        <a href="" class="w3-bar-item w3-button"><i class="fa fa-cog"></i></a>
+      </div>
     </div>
+    <hr/>
+    <div class="w3-container">
+      <h5>Dashboard</h5>
+      <div class="divider"></div>
+      <div class="w3-bar-block">
+        <a href="#" class="w3-bar-item w3-button w3-padding-16 w3-hide-large w3-dark-grey w3-hover-black" onclick="w3_close();" title="close menu">
+          <i class="fa fa-remove fa-fw"></i>Close Menu 
+        </a>
+        <div class="w3-dropdown-hover">
+          <button class="w3-button w3-bar-item">Testy<i class="fa fa-caret-down w3-right"></i></button>
+          <div class="w3-dropdown-content w3-bar-block w3-card-4">
+            <a href="" class="w3-bar-item w3-button">Lista</a>
+            <a href="" class="w3-bar-item w3-button">Nowy</a>
+            <a href="" class="w3-bar-item w3-button">Edycja</a>
+            <a href="" class="w3-bar-item w3-button">Szukaj</a>
+          </div>
+        </div>
+        <div class="w3-dropdown-hover">
+          <button class="w3-bar-item w3-button">Metale i stopy<i class="fa fa-caret-down w3-right"></i></button>
+          <div class="w3-dropdown-content w3-bar-block w3-border">
+            <a href="" class="w3-bar-item w3-button">Lista</a>
+            <a href="" class="w3-bar-item w3-button">Nowy</a>
+            <a href="" class="w3-bar-item w3-button">Edycja</a>
+            <a href="" class="w3-bar-item w3-button">Szukaj</a>
+          </div>
+        </div>
+        <div class="w3-dropdown-hover">
+          <button class="w3-bar-item w3-button">Podłoża<i class="fa fa-caret-down w3-right"></i></button>
+          <div class="w3-dropdown-content w3-bar-block w3-border">
+            <a href="" class="w3-bar-item w3-button">Lista</a>
+            <a href="" class="w3-bar-item w3-button">Nowy</a>
+            <a href="" class="w3-bar-item w3-button">Edycja</a>
+            <a href="" class="w3-bar-item w3-button">Szukaj</a>
+          </div>
+        </div>
+        <a href="" class="w3-bar-item w3-button">Aparatura</a>
+        <a href="" class="w3-bar-item w3-button">Procedury</a>
+        @if (auth()->user()->is_admin())
+          <a href="" class="w3-bar-item w3-button">Użytkownicy</a>
+        @endif
+      </div>
+    </div>
+  </nav>
+  <div class="w3-overlay w3-hide-large w3-animate-opacity" onclick="w3_close()" style="cursor: pointer" title="close side menu" id="myOverlay"></div>
+
+  <!--zawartość strony -->
+  <div class="w3-main main-content">
+    @yield('content')
+  </div> 
+<script>
+  //FIXME: Na etapie projektowania i wprowadzania zmian wszystkie JS będą poniżej. 
+  //W późniejszym terminie lub jak się za bardzo rozrosną to zostaną przerzucone do nowego pliku.
+  const mySidebar = document.getElementById("mySidebar");
+  const overlayBg = document.getElementById("myOverlay");
+  function w3_open(){
+    if(mySidebar.style.display === "block"){
+      mySidebar.style.display="none";
+      overlayBg.style.display="none";
+    } else {
+      mySidebar.style.display="block";
+      overlayBg.style.display="block";
+    }
+  }
+  function w3_close(){
+    mySidebar.style.display="none";
+    overlayBg.style.display="none";
+  }
+
+
+</script>
 </body>
 </html>
--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,4 +1,28 @@
 @extends('dashboard')
+@section('page_title')
+  {{"WWP-CBM Index"}}
+@endsection
+@section('header')
+  <header class="w3-container page-header">
+    <h5><b><i class="fa fa-dashboard"></i>Lista testów zwilżalności</b></h5>
+    @if (Session::has('error'))
+      <div class="w3-panel w3-pale-red w3-card">
+        <span onclick="this.parentElement.style.display='none'"
+        class="w3-button w3-large w3-display-topright">&times;</span>
+        <h3>Błąd</h3>
+        <p>{{Session::get('error')}}</p>
+      </div>
+    @endif
+    @if (Session::has('success'))
+      <div class="w3-panel w3-pale-green w3-card">
+        <span onclick="this.parentElement.style.display='none'"
+        class="w3-button w3-large w3-display-topright">&times;</span>
+        <h3>Błąd</h3>
+        <p>{{Session::get('success')}}</p>
+      </div>
+    @endif
+  </header>
+@endsection
 @section('content')
-    <h1>wczytanie z <span>dashboard/index.blad.php</span></h1>
+  
 @endsection
