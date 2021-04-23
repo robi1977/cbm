19a252c7a8e964d18fc7ed3ed02f5a0c65c9ee2f
Rafal <rafal.robi.nowak@gmail.com>
2021-04-23 09:30:27 +0200

    merge 2 heads on gits

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,26 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<<<<<<< HEAD
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>Dashboard - {{ config('app.name', 'Laravel') }}</title>
-
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-=======
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -39,7 +19,6 @@
   <link rel="stylesheet" href="{{asset('css/all.css')}}">
   <link rel="stylesheet" href="{{asset('css/app.css')}}">
   <script src="{{asset('js/all.js')}}"></script>
->>>>>>> 0b295a64b261a3e6df85e24accf6c160788ca305
 </head>
 <body class="w3-light-grey">
   <!--Górny pasek z logo i przyciskiem na menu -->
